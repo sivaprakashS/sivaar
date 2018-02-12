fe804f40e6733cce1aff0f682ff211c2d7376f56
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-02-12 15:43:11 +0530

    count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[10];
+int i = 0, l = 0, f = 1;
+puts("Enter any string");
+gets(str);
+for(i = 0;i<=;i++)
+{
+l = l + 1;
+}
+printf("The number of characters in the string are %d", l);
+}
